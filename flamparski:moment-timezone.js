4e970c37d84b2979a5efb960bb2304102e00041f
Filip Wieland <filiplamparski@gmail.com>
2015-02-11 13:14:43 +0000

    the great component update

--- flamparski:moment-timezone.js
+++ flamparski:moment-timezone.js
@@ -1,1 +1,2 @@
-// Write your package code here!
+moment = this.moment;
+delete this.moment;
--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
   summary: "A Meteor 0.9 package for moment-timezone",
-  version: "0.0.1",
+  version: "0.1.0",
   git: "https://github.com/FLamparski/flamparski-moment-timezone"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
   api.addFiles(['./bower_components/moment-timezone/builds/moment-timezone-with-data-2010-2020.js'], ['server', 'client']);
-  // uh... expose?
+  api.addFiles(['./flamparski:moment-timezone.js'], ['server', 'client']);
+  api.export(['moment']);
 });
 
 Package.onTest(function(api) {
